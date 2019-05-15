57f79943fd3b9ec8d5f68da65cf615e88dddbc3a
David <david-nader@users.noreply.github.com>
2019-05-15 02:48:37 +0200

    Implement alpha-beta search

---
+++ abs.h
@@ -0,0 +1,20 @@
+/*
+    Alpha-Beta Search
+*/
+#ifndef ABS_H
+#define ABS_H
+
+#include "state.h"
+#include <vector>
+
+#define neg_inf (piece::them - 1)
+#define pos_inf (piece::us + 1)
+
+//todo: move getChildren function to State class
+void getChildren(const State& state, std::vector<State>& children);
+
+State abs(const State& state);
+piece max_value(const State& state, int alpha, int beta, State* max_state);
+piece min_value(const State& state, int alpha, int beta);
+
+#endif //ABS_H
